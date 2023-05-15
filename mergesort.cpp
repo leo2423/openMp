a405b03e42285882e1988271dd26927eedfe7029
leo2423 <133692569+leo2423@users.noreply.github.com>
2023-05-15 23:00:42 +0530

    Add files via upload

---
+++ mergesort.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+#include <omp.h>
+
+using namespace std;
+
+void merge(int arr[], int left, int mid, int right) {
+    int i, j, k;
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    
+    int L[n1], R[n2];
+    
+    for (i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[mid + 1 + j];
+        
+    i = 0;
+    j = 0;
+    k = left;
+    
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+    
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        
+        #pragma omp parallel sections
+        {
+            #pragma omp section
+            {
+                mergeSort(arr, left, mid);
+            }
+            
+            #pragma omp section
+            {
+                mergeSort(arr, mid + 1, right);
+            }
+        }
+        
+        merge(arr, left, mid, right);
+    }
+}
+
+int main() {
+    int arr[] = {12, 11, 13, 5, 6, 7};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    
+    cout << "Given array is: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    
+    mergeSort(arr, 0, n - 1);
+    
+    cout << "\nSorted array is: ";
+    for (int i = 0; i < n; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+    
+    return 0;
+}
